597e7c4ea7326d29a8e9a76c2763d227f09600f3
vultorz <70312693+vultorz@users.noreply.github.com>
2021-03-07 09:42:57 -0600

    Add files via upload

---
+++ league.py
@@ -0,0 +1,13 @@
+import wmi; import os; import time
+
+w = wmi.WMI()
+
+while 1 > 0:
+    for process in w.Win32_Process():
+        if process.name == "LeagueClient.exe":
+            process.Terminate()
+            print("stopping league")
+            os.system("start \"\" https://www.healthygamer.gg/why-do-video-games-make-you-angry/")
+            print("link opened")
+    time.sleep(10)
+
