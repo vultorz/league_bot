adad497fb5ed3e2bcaf9f08875b1cf609a54d4d4
vultorz <70312693+vultorz@users.noreply.github.com>
2021-03-07 18:12:14 -0600

    Update league.py

--- league.py
+++ league.py
@@ -9,5 +9,4 @@ while 1 > 0:
             print("stopping league")
             os.system("start \"\" https://www.healthygamer.gg/why-do-video-games-make-you-angry/")
             print("link opened")
-    time.sleep(10)
-
+    time.sleep(3)
